b19a97182db3e8c14d5d1465bdebe1082b8be538
Zar Chi Oo <zarchioo@my.jcu.edu.au>
2022-12-13 16:02:34 +0630

    Change the file read method, modify menu function , implement exception handling for file reading

--- assignment1.py
+++ assignment1.py
@@ -4,43 +4,52 @@ Name:Zar Chi Oo
 Date started:12/12/2022
 GitHub URL:https://github.com/JCUS-CP1404/assignment-1-ZarChi123.git
 """
-import csv
+from operator import itemgetter
+
+FILENAME = "books1.csv"
 
 
 def main():
     books = []
     print("Reading Tracker 1.0 - Zar Chi Oo")
-    books = read_csv("books.csv")
-    print("{} books loaded".format(len(books)))
-    print_menu()
-    choice = input(">>>")
-    while choice != "Q":
-        if choice == "L":
-            pass
-        elif choice == "A":
-            pass
-        elif choice == "M":
-            pass
-        print_menu()
+    menu_string = "Menu:\nL-List all books\nA-Add new book\nM-Mark a book as completed\nQ-Quit"
+    get_books(FILENAME, books)
+    if len(books) > 0:
+        print("{} books loaded".format(len(books)))
+        print(menu_string)
         choice = input(">>>")
-    print("Bye")
+        while choice != "Q":
+            if choice == "L":
+                list_books(books)
+            elif choice == "A":
+                pass
+            elif choice == "M":
+                pass
+            print(menu_string)
+            choice = input(">>>")
+        print("Finish")
 
 
-def read_csv(filename):
-    """Read the book csv file"""
-    books = []
-    in_file = open(filename, "r")
-    csv_file = csv.reader(in_file)
-    for line in csv_file:
-        books.append(list(line))
-    in_file.close()
-    return books
+def list_books(books):
+    """Print list of books sorted by author and title"""
+    print(books)
+    books.sort(key=itemgetter(1, 0))
+    print(books)
 
 
-def print_menu():
-    """Print the menu options """
-    menu_options = "Menu:\nL-List all books\nA-Add new book\nM-Mark a book as completed\nQ-Quit"
-    print(menu_options)
+def get_books(filename, books):
+    """Read the book csv file and split them into separate details"""
+    try:
+        with open(filename, "r", encoding="utf-8-sig") as in_file:
+            for line in in_file:
+                # print(line)
+                # print(type(line))
+                book_details = line.strip().split(",")
+                books.append(list(line))
+                # print(book_details)
+                # print(type(book_details))
+    except FileNotFoundError:
+        print(f"The file \"{filename}\" was not found!")
 
 
 main()
